1f745f6bc13b43858cfd36f8409025de39c4ff4f
Philipe <philipe-leandro@hotmail.com>
2022-01-19 09:24:53 -0300

    Add polimorfism exercise

---
+++ Polimorfism/Watching_movie.rb
@@ -0,0 +1,29 @@
+class Cinema
+  attr_reader :movie
+  
+  def initialize(movie)
+    @movie = movie    
+  end
+
+  def watch
+    puts "I'm watching #{@movie} movie!!"
+  end
+end
+
+class Tv < Cinema
+  def watch
+    puts "Now I'm at home and watching #{@movie} movie!!"
+  end
+end
+
+class Cellphone < Cinema
+end
+
+spider_man = Cinema.new("iron man")
+spiderman = Tv.new("hulk")
+sp = Cellphone.new("Hobbit")
+
+spider_man.watch
+spiderman.watch
+sp.watch
+puts "---------------------"
